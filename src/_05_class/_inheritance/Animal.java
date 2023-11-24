86bab6dd194811c7fb3ffc54ea8882ce3e7b3779
sy33002 <sy33002@naver.com>
2023-11-24 19:00:18 +0900

    feat: 수업 종료

--- src/_05_class/_inheritance/Animal.java
+++ src/_05_class/_inheritance/Animal.java
@@ -2,7 +2,37 @@ package _05_class._inheritance;
 
 public class Animal {
     // 부모 클래스의 메소드
+
+    private String breed;
+    private int age;
+    private String name;
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
     void makeSound() {
-        System.out.println("동물이 운다");
+        System.out.println("동물은 소리를 낸다.");
     }
 }
